7cf13a61c4786158260f457a2ccf2aff8af627a3
Builder106 <vaughanolayinka@gmail.com>
2025-09-04 10:02:02 +0100

    Restart of initial layout

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MindfulMuse.Client;
+using MindfulMuse;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
